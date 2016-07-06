ec42a22ed031c4d64af23d1b374466f684133c9e
HenryBucklow <>
2016-07-06 23:01:00 +0200

    version 0.3

--- test/Tests.hs
+++ test/Tests.hs
@@ -4,8 +4,10 @@ import Data.Digits
 import Text.Printf
 import Test.QuickCheck
 
+tests :: [([Char], IO ())]
 tests = [
     ("checkDigitsRoundTrip", quickCheck prop_digitsRoundTrip)]
 
+main :: IO ()
 main = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
 
